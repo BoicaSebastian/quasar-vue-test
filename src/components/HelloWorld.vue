56d839dcbd364f1f9d215a436ce9208b0b68b933
Boica Sebastian <boicasebastian@gmail.com>
2026-01-12 15:25:10 +0200

    feat: add pinia as a state management

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { useCounterStore } from '../stores/counter'
 
 defineProps<{
   msg: string
 }>()
 
-const count = ref(0)
+const counterStore = useCounterStore()
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="counterStore.increment()">count is {{ counterStore.count }}</button>
+    <p>Double count: {{ counterStore.doubleCount }}</p>
+    <div style="margin-top: 10px;">
+      <button type="button" @click="counterStore.decrement()" style="margin-right: 8px;">Decrement</button>
+      <button type="button" @click="counterStore.reset()">Reset</button>
+    </div>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
