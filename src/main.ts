87fef17a3d7c2c79cf0a490553fb2c2d9d4fdeaf
Boica Sebastian <boicasebastian@gmail.com>
2026-01-13 11:36:22 +0200

    feat: add vue3-toastify module

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { Quasar } from 'quasar'
 import router from './router'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
 
 // Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
 
 // Import Quasar css
 import 'quasar/dist/quasar.css'
+
+// Import Vue3Toastify css
+import 'vue3-toastify/dist/index.css'
 
 import './style.css'
 import App from './App.vue'
@@ -22,4 +26,7 @@ app.use(Quasar, {
 })
 app.use(pinia)
 app.use(router)
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+} as ToastContainerOptions)
 app.mount('#app')
